889a29b84ac29b81876658a2fa49172dc1eb9fa7
nimclient <niw.master@gmail.com>
2015-03-10 16:34:46 +0900

    fabfile エラーを修正

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -3,7 +3,7 @@ import fabric.api as fab
 
 
 def dev_reload():
-    with fab.cd("/home/nimclient/test")
+    with fab.cd("/home/nimclient/test"):
         fab.sudo("git pull", user="nimclient")
         fab.sudo("pip install -U -r requirements/requirements.txt")
     fab.sudo("echo done")
