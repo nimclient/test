78efb194d786c9579c0ad10e698ee3361c37fcc7
nimclient <niw.master@gmail.com>
2015-03-10 17:52:17 +0900

    travis yml修正

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -6,5 +6,4 @@ def dev_reload():
     with fab.cd("/home/nimclient/test"):
         fab.sudo("git pull", user="nimclient")
         fab.sudo("pip install -U -r requirements/requirements.txt")
-    fab.sudo("echo done")
 
